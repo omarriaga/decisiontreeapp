0ea30edda38ead8bd4ef71c1062892c6eaf7eb5b
Leo Betancourt <leobeta@gmail.com>
2019-05-22 13:54:46 -0500

    changes on models

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,7 +1,24 @@
+require 'scoruby'
+
+
 class HomeController < ApplicationController
   def form
+
   end
 
   def validate
+    features = transform(params)
+    decision_tree = Scoreruby.load_model('decision_tree.pmml')
+    @decision = decision_tree.decide(features)
+  end
+
+  private
+
+  def transform(params)
+    {
+      'TIENE_BANOS': params['banos']
+    }
   end
 end
+
+
